90d9c8cf113fcdbd63ef5f0cb0bae20ab1a1bd74
Reza Zeraat <rezacr588@gmail.com>
2021-07-23 02:44:05 +0430

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string | undefined;
+  date: string | undefined;
+  amount: number | undefined;
+  height: number | undefined;
+  miles: number | undefined;
+  onMilesChange(event: Event) {
+    let milesValue: string = (<HTMLInputElement>event.target).value;
+    this.miles = parseFloat(milesValue)
+  }
+  onNameChange(event: Event) {
+    let nameValue: string = (<HTMLInputElement>event.target).value;
+    this.name = nameValue
+  }
+  onDateChange(event: Event) {
+    let dateValue: string = (<HTMLInputElement>event.target).value;
+    this.date = dateValue
+  }
+  onAmountChange(event: Event) {
+    let amountValue: string = (<HTMLInputElement>event.target).value;
+    this.amount = parseFloat(amountValue)
+  }
+  onHeightChange(event: Event) {
+    let heightValue: string = (<HTMLInputElement>event.target).value;
+    this.height = parseFloat(heightValue)
+  }
 }
